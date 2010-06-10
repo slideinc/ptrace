e3575786acf507beeadec1434789942c4be0a75f
Travis J Parker <travis.parker@gmail.com>
2010-06-10 09:43:54 -0700

    license, packaging etc

---
+++ pavement.py
@@ -0,0 +1,61 @@
+import errno
+import os
+from setuptools import Extension
+
+from paver.easy import *
+from paver.path import path
+from paver.setuputils import setup
+
+
+setup(
+    name="ptrace",
+    description="",
+    version="1.0",
+    ext_modules=[Extension(
+        'ptrace',
+        ['ptracemodule.c'],
+        include_dirs=('.',),
+        extra_compile_args=['-Wall'])],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        #"License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: C",
+        "Topic :: System :: Networking",
+    ]
+)
+
+MANIFEST = (
+    "LICENSE",
+    "setup.py",
+    "paver-minilib.zip",
+    "ptracemodule.c",
+)
+
+@task
+def manifest():
+    path('MANIFEST.in').write_lines('include %s' % x for x in MANIFEST)
+
+@task
+@needs('generate_setup', 'minilib', 'manifest', 'setuptools.command.sdist')
+def sdist():
+    pass
+
+@task
+def clean():
+    for p in map(path, ('ptrace.egg-info', 'dist', 'build', 'MANIFEST.in')):
+        if p.exists():
+            if p.isdir():
+                p.rmtree()
+            else:
+                p.remove()
+    for p in path(__file__).abspath().parent.walkfiles():
+        if p.endswith(".pyc") or p.endswith(".pyo"):
+            try:
+                p.remove()
+            except OSError, exc:
+                if exc.args[0] == errno.EACCES:
+                    continue
+                raise
