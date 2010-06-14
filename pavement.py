dbfe88307ada9e457882d19966011a28499981fc
Travis J Parker <travis.parker@gmail.com>
2010-06-14 15:44:15 -0700

    author info

--- pavement.py
+++ pavement.py
@@ -12,6 +12,8 @@ setup(
     description="",
     version="1.0",
     license="bsd",
+    author="Libor Michalek",
+    author_email="libor@pobox.com",
     ext_modules=[Extension(
         'ptrace',
         ['ptracemodule.c'],
