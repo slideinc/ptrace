c44d260272db001fc2fa4ca343657b7891c73880
Travis J Parker <travis.parker@gmail.com>
2010-06-15 11:02:41 -0700

    packaging fix and version bump

--- pavement.py
+++ pavement.py
@@ -10,7 +10,7 @@ from paver.setuputils import setup
 setup(
     name="ptrace",
     description="C extension providing access to ptrace(2)",
-    version="1.0",
+    version="1.0.1",
     license="bsd",
     author="Libor Michalek",
     author_email="libor@pobox.com",
